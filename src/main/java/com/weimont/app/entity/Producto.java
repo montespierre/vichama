bcd30c1bee2af24620ff5ef256c616514f0e9888
Tu Nombre <montespierre@gmail.com>
2021-01-01 15:47:27 -0500

    segunda subida

--- src/main/java/com/weimont/app/entity/Producto.java
+++ src/main/java/com/weimont/app/entity/Producto.java
@@ -3,10 +3,14 @@ package com.weimont.app.entity;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="productos")
 public class Producto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
